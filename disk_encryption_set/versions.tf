fa890e8866015a5124ebb6663ba647157b56f30f
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-27 11:27:09 +0530

    Update versions.tf

--- disk_encryption_set/versions.tf
+++ disk_encryption_set/versions.tf
@@ -4,7 +4,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "~> 3.71"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
     }
   }
 }
